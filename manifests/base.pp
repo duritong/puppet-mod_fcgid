0a0bd1e061f705957ab977a1b091db1c621a8c2d
mh <mh@immerda.ch>
2009-10-04 22:09:02 +0200

    factor everything out

---
+++ manifests/base.pp
@@ -0,0 +1,19 @@
+class mod_fcgid::base {
+    package{'mod_fcgid':
+        ensure => installed,
+    }
+
+    file{"/etc/httpd/conf.d/fcgid.conf":
+        source => [ "puppet://$server/files/mod_fcgid/${fqdn}/ssl.conf",
+                    "puppet://$server/files/mod_fcgid/fcgid.conf",
+                    "puppet://$server/mod_fcgid/fcgid.conf"
+        ],
+        owner => root, group => 0, mode => 0644;
+    }
+    file{"/etc/httpd/conf.d/php.conf":
+        ensure => absent
+    }
+    file{"/etc/httpd/conf.d/perl.conf":
+        ensure => absent
+    }
+}
--- manifests/init.pp
+++ manifests/init.pp
@@ -2,50 +2,8 @@
 # Copyright (C) 2007 admin@immerda.ch
 # GPLv3
 
-# modules_dir { "mod_fcgid": }
-
 class mod_fcgid {
     case $operatingsystem {
-        centos: { include mod_fcgid::centos }
         default: { include mod_fcgid::base }
     }
 }
-
-class mod_fcgid::base {
-    $config_dir = '/etc/httpd/'
-
-    package{'mod_fcgid':
-        ensure => installed,
-    }
-
-    if $selinux {
-        include mod_fcgid::selinux
-    }
-
-    file{"${config_dir}/conf.d/fcgid.conf":
-        source => [ "puppet://$server/files/mod_fcgid/${fqdn}/ssl.conf",
-                    "puppet://$server/files/mod_fcgid/fcgid.conf",
-                    "puppet://$server/mod_fcgid/fcgid.conf"
-        ],
-        owner => root, group => 0, mode => 0644;
-    }
-    file{"${config_dir}/conf.d/php.conf":
-        ensure => absent
-    }
-    file{"${config_dir}/conf.d/perl.conf":
-        ensure => absent
-    }
-
-
-#    service{mod_fcgid:
-#        ensure => running,
-#        enable => true,
-#        #hasstatus => true, #fixme!
-#        require => Package[mod_fcgid],
-#    }
-
-}
-
-class mod_fcgid::centos inherits mod_fcgid::base {
-}
-
