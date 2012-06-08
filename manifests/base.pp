6d67d46c8f1d9502aeda964c14b6f2aaee88ee9b
mh <mh@immerda.ch>
2012-06-08 12:48:57 -0300

    refactor things for >2.7

--- manifests/base.pp
+++ manifests/base.pp
@@ -1,30 +1,30 @@
 class mod_fcgid::base {
-    package {
-        'mod_fcgid' :
-            ensure => installed,
+  package {
+    'mod_fcgid' :
+      ensure => installed,
+  }
+  file {
+    '/var/www/mod_fcgid-starters' :
+      ensure => directory,
+      force => true,
+      purge => true,
+      recurse => true,
+      owner => root,
+      group => 0,
+      mode => 0644 ;
+  }
+  if $::selinux == 'true' {
+    if $::lsbmajdistrelease == '5' {
+      package {
+        'mod_fcgid-selinux' :
+          ensure => present,
+          require => Package['mod_fcgid'],
+      }
     }
-    file {
-        '/var/www/mod_fcgid-starters' :
-            ensure => directory,
-            force => true,
-            purge => true,
-            recurse => true,
-            owner => root,
-            group => 0,
-            mode => 0644 ;
+    selinux::fcontext {
+      '/var/www/mod_fcgid-starters/.+(/.*)?' :
+        setype => 'httpd_sys_script_exec_t',
+        before => File['/var/www/mod_fcgid-starters'] ;
     }
-    if $::selinux == 'true' {
-        if $::lsbmajdistrelease == '5' {
-            package {
-                'mod_fcgid-selinux' :
-                    ensure => present,
-                    require => Package['mod_fcgid'],
-            }
-        }
-        selinux::fcontext {
-            '/var/www/mod_fcgid-starters/.+(/.*)?' :
-                setype => 'httpd_sys_script_exec_t',
-                before => File['/var/www/mod_fcgid-starters'] ;
-        }
-    }
+  }
 }
