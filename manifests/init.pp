f58e06afc83171042030d3d938a1796d6563006a
mh <mh@immerda.ch>
2021-02-06 22:27:49 +0100

    modernize module

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,9 +1,25 @@
-# Copyright (C) 2007 admin@immerda.ch
-# GPLv3
-
-# manage mod_fcgid installation
+# setup all the things we
+# need for mod_fcgid
 class mod_fcgid {
-  case $::operatingsystem {
-    default: { include mod_fcgid::base }
+  package {
+    'mod_fcgid' :
+      ensure  => installed,
+  } -> file {
+    '/var/www/mod_fcgid-starters' :
+      ensure  => directory,
+      force   => true,
+      purge   => true,
+      recurse => true,
+      require => Package['apache'],
+      owner   => root,
+      group   => 0,
+      mode    => '0644';
+  }
+  if $facts['os']['selinux']['enabled'] {
+    selinux::fcontext {
+      '/var/www/mod_fcgid-starters/.+(/.*)?' :
+        setype => 'httpd_sys_script_exec_t',
+        before => File['/var/www/mod_fcgid-starters'];
+    }
   }
 }
